cdd64f5214e7ce309acdec6ba1d3384e726a0f31
abualhassanali <abualhassan.ali@switch.com.iq>
2026-08-30 16:39:50 +0300

    day 2: Add EF Core DbContext and connect to database

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalogApi.Models;
+
+namespace ProductCatalogApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
+    }
+}
